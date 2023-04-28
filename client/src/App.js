57cae993131096846b71377aa3a2bfb61a017b92
SanjuBalasubramani <sanjubalasubramani@gmail.com>
2023-04-28 10:03:59 +0530

    backend_connect_before

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,28 @@
-
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import Contact from './components/pages/Contact';
-import Ser from './components/ser';
-// import Login from './components/Login';
-// import { Register } from './components/Register';
-import About from './components/About';
-import Account from './components/pages/Account';
-// import Layout from './components/layout';
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Contact from "./components/pages/Contact";
+import About from "./components/About";
+import Account from "./components/pages/Account";
 function App() {
   return (
     <>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />;
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+      />
+      
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={Account} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/si' component={Ser} />
-          
-          {/* <Route path='/Layout' component={Layout}/> */}
-          <Route path='/about' component={About}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={Account} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
         </Switch>
       </Router>
     </>
